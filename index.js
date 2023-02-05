bc1dd5de2e7f0fca0ef51976c5c3fa8f21999201
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-02-05 22:54:35 +0600

    sendgrid_Ekta_Kuttar_Baccha_Oi_Kuttar_bacchare_bad_Dilam

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
-const nodemailer = require("nodemailer");
+const nodeMailer = require("nodemailer");
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
@@ -10,9 +10,6 @@ const stripe = require("stripe")(process.env.STRIPE_TEST_SECRET_KEY)
 // Middle Ware
 app.use(cors());
 app.use(express.json());
-// Sendgrid
-const sgMail = require("sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
@@ -51,9 +48,19 @@ async function run() {
 			})
 		};
 		// Send Mail to Customer after Cinfirm Booking
-		const SendBookingEmail = (booking) => {
+		const SendBookingEmail = async (booking) => {
 			const { email, treatmentName, slot, name, date } = booking;
-			sgMail.sendMail({
+			nodeMailer.createTransport({
+				host: 'tanjimulislamsabbir02',
+				port: 456,
+				secure: true,
+				auth: {
+					user: "tanjimulislamsabbir02",
+					pass: 'TanjimSabbir<Away2022>G'
+				}
+			})
+
+			sendMail({
 				from: "tanzimulislamsabbir@outlook.com", // verified sender email
 				to: email, // recipient email
 				subject: `Your Appointment for ${treatmentName} is Confirmed`, // Subject line
